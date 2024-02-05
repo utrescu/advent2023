e3ea4d9a40229020ac9e120ce65daf7712da0260
Xavier Sala <utrescu@gmail.com>
2024-02-05 16:32:23 +0100

    fix(day1): simplificació del day 01 in Rust
    Simplifico i dóno format al codi. No tinc clar que sigui millor que l'anterior.
    He fet les dues versions de forma diferent, però totes dues es podrien fer com es fa en la segona

--- day01rust/src/main.rs
+++ day01rust/src/main.rs
@@ -1,7 +1,13 @@
-use std::{fs::File, io::{self, BufRead}, path::Path};
+use std::{
+    fs::File,
+    io::{self, BufRead},
+    path::Path,
+};
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
@@ -10,7 +16,7 @@ fn main() {
     let filename = "./input";
 
     let mut inputs = Vec::new();
-    
+
     if let Ok(lines) = read_lines(filename) {
         for line in lines.flatten() {
             inputs.push(line)
@@ -20,7 +26,6 @@ fn main() {
     // Part 1
     let mut result1 = 0;
     for input in &inputs {
-        
         let first_number = input.chars().find(|x| x.is_digit(10));
         let x1 = first_number.unwrap_or_else(|| ' ');
         let last_number = input.chars().rev().find(|x| x.is_digit(10));
@@ -28,40 +33,35 @@ fn main() {
 
         result1 = result1 + create_number(x1, x2);
     }
-
     println!("Part1: {}", result1);
-
 
     // Part 2
     let mut result2 = 0;
-    let digits_in_words = vec!{
+    let digits_in_words = vec![
+        ("1", '1'),
+        ("2", '2'),
+        ("3", '3'),
+        ("4", '4'),
+        ("5", '5'),
+        ("6", '6'),
+        ("7", '7'),
+        ("8", '8'),
+        ("9", '9'),
         ("one", '1'),
         ("two", '2'),
         ("three", '3'),
         ("four", '4'),
-        ("five",'5'),
-        ("six",'6'),
-        ("seven",'7'),
-        ("eight",'8'),
-        ("nine",'9'),
-    };
+        ("five", '5'),
+        ("six", '6'),
+        ("seven", '7'),
+        ("eight", '8'),
+        ("nine", '9'),
+    ];
 
     for input in &inputs {
-
         let vec_chars: Vec<char> = input.chars().collect();
-        // First numbers
-        let first = vec_chars.iter().position(|x| x.is_digit(10));
-        let (mut first_position, mut first_value)= match first {
-            Some(n) => { (n, vec_chars[n])  },
-            None => (vec_chars.len()+1, ' '),
-        };
-        // Last numbers
-
-        let last = vec_chars.iter().rposition(|x: &char| x.is_digit(10));
-        let (mut last_position, mut last_value) = match last {
-            Some(n) => (n, vec_chars[n]),
-            None => (0, ' '),
-        };
+        let (mut first_position, mut first_value) = (vec_chars.len() + 1, ' ');
+        let (mut last_position, mut last_value) = (0usize, ' ');
 
         for digit_in_word in digits_in_words.iter() {
             let first_digit = input.find(digit_in_word.0);
@@ -73,13 +73,11 @@ fn main() {
                         (first_position, first_value)
                     };
                     (result, value)
-
-                },
+                }
                 None => (first_position, first_value),
             };
 
             // Last
-
             let last_digit = input.rfind(digit_in_word.0);
             (last_position, last_value) = match last_digit {
                 Some(number) => {
@@ -89,22 +87,13 @@ fn main() {
                         (last_position, last_value)
                     };
                     (result, value)
-
-                },
+                }
                 None => (last_position, last_value),
             };
-
-            // let last_number = input.chars().rev().find(|x| x.is_digit(10)); 
-            
-            
         }
-
         result2 = result2 + create_number(first_value, last_value);
-        
     }
     println!("Part2: {}", result2);
-
-
 }
 
 fn create_number(x1: char, x2: char) -> i32 {
